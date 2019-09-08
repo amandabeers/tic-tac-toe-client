1e8d5b4108e5ef3ef0c85fe577caa3c886f23aca
amandabeers <amanda.green413@gmail.com>
2019-09-08 14:26:55 -0400

    Update api url
    -Updated api url to point to the redeployed game api

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://tic-tac-toe-wdi-production.herokuapp.com',
-  development: 'https://tic-tac-toe-wdi.herokuapp.com/'
+  production: 'https://radiant-sea-43159.herokuapp.com',
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
